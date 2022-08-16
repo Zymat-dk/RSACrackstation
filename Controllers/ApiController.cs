99a96d015482495337d8c2c4956f8501ddd20fff
DNIIBOY <daniel.nettelfield@gmail.com>
2022-08-16 18:30:47 +0200

    First working version of GetFactors

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -7,10 +7,12 @@ using System.Text.Json.Nodes;
 namespace RSACrackstation.Controllers;
 
 public class ApiController : Controller {
-    public int[] GetFactors(string inputNum) {
-        var url = $"http://factordb.com/api?query={inputNum}";
+    public string[] GetFactors(string inputNum) {
+        string[] factors = { "-1", "-1" };  // Default values if the number, or the factors are not prime
+        
+        var url = $"http://factordb.com/api?query={inputNum}";  // factordb api url
         var request = WebRequest.Create(url);
-        request.Method = "GET";
+        request.Method = "GET";  // Use GET request
 
         using var webResponse = request.GetResponse();
         using var webStream = webResponse.GetResponseStream();
@@ -18,12 +20,23 @@ public class ApiController : Controller {
         using var reader = new StreamReader(webStream);
         var data = reader.ReadToEnd();
 
-        var jsonData = JsonObject.Parse(data);
+        dynamic jsonData = JsonObject.Parse(data);  // parse json data
 
-        Console.WriteLine(jsonData);
+        if (jsonData["status"].ToString() != "FF" || jsonData["factors"].Count != 2) {
+            // If the number is prime, or has more than two factors, return -1 -1
+            return factors;
+        }
 
-        int[] arr = { -1, -1 };
+        if (jsonData["factors"][0][1].ToString() != "1" || jsonData["factors"][1][1].ToString() != "1") {
+            // Check for multiple of the same factor, and return -1 -1 if found
+            return factors;
+        }
+        
+        for(var i=0; i<2; i++){
+            // Get the two factors from the json data
+            factors[i] = jsonData["factors"][i][0].ToString();
+        }
 
-        return arr;
+        return factors;
     }
 }
