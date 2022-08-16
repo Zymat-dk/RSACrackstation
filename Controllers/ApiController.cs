82f518f1692d7955a51ef23da35ffd943812ee71
Gustav2 <gustavnybro@protonmail.com>
2022-08-16 17:48:56 +0200

    Sumbit on enter + reformat

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -6,7 +6,7 @@ namespace RSACrackstation.Controllers;
 
 public class ApiController : Controller{
     public int[] GetFactors(string inputNum){
-        int[] nums = {-1, -1};
+        int[] nums = { -1, -1 };
         return nums;
     }
 }
