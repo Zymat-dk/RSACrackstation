d8a17b057a9876bb9420140b11bc46fb7683dd5f
gustav2 <gustavnybro@protonmail.com>
2022-08-16 12:27:54 +0200

    Init commit + tailwind added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RSACrackstation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
