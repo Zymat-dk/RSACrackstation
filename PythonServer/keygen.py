a0fb5ae350a7012d13454e28b07891e1572d6e11
DNIIBOY <daniel@nettelfield.com>
2022-11-18 12:51:21 +0100

    add smalle.py

--- PythonServer/keygen.py
+++ PythonServer/keygen.py
@@ -1,5 +1,6 @@
 from Crypto.Util import number
 
+
 def generatePrimes(size: int) -> tuple:
     """
     Generate two primes of size {size}
@@ -21,9 +22,8 @@ def main():
         main()
     numbers, status, is_strong = generatePrimes(size)
     p, q = numbers
-    print(f"{'Strong' if is_strong else 'Weak'} key\np: {p}\nq: {q}\nn: {p*q}")
+    print(f"{'Strong' if is_strong else 'Weak'} key\np: {p}\nq: {q}\nn: {p * q}")
 
 
 if __name__ == "__main__":
     main()
-
--- PythonServer/smalle.py
+++ PythonServer/smalle.py
@@ -1,2 +1,32 @@
+import gmpy2
+
+MAX_I = 10000
+MAX_E = 10
+
+
+def smallE(n: int, e: int, c: int) -> dict:
+    response = {"success": False}
+    if e > MAX_E:
+        return response
+    for i in range(MAX_I):
+        m, is_true_root = gmpy2.iroot(i * n + c, e)
+        if is_true_root:
+            response["success"] = True
+            response["i"] = i
+            response["m"] = bytearray.fromhex(format(m, 'x')).decode()
+            break
+
+    return response
+
+
+def main():
+    n = int(input("n: "))
+    e = int(input("e: "))
+    c = int(input("c: "))
+    response = smallE(n, e, c)
+    if response["success"]:
+        print(f"i: {response['i']}\nm: {response['m']}")
+
+
 if __name__ == "__main__":
-    pass
+    main()
