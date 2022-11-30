28c9f807524d048e1ca00b89f3b804095f9c5a9a
Gustav <gustav@gustavnybro.com>
2022-12-01 00:51:27 +0100

    Added small e page + routes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -49,6 +49,10 @@ public class HomeController : Controller{
         return View();
     }
     
+    public IActionResult SmallE(){
+        return View();
+    }
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error(){
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
